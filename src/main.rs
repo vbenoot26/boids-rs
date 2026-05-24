b9c0d481452b9f6b19d76a97c44f5dc221272bfd
vincent benoot <vincent.benoot@gmail.com>
2026-05-24 19:24:24 +0200

    just move the ball

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
 use raylib::prelude::{Color, RaylibDraw};
 
+struct Boid {
+    x: i32,
+    y: i32,
+
+    speedx: i32,
+    speedy: i32,
+}
+
+impl Boid {
+    fn step(&mut self) {
+        self.x += self.speedx;
+        self.y += self.speedy;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
-    let (mut rl, thread) = raylib::init().size(640, 480).build();
+
+    let width = 640;
+    let height = 480;
+    let (mut rl, thread) = raylib::init().size(width, height).build();
+
+    rl.set_target_fps(60);
+
+    let mut boid = Boid {
+        x: 12,
+        y: 12,
+        speedx: 10,
+        speedy: 5,
+    };
 
     while !rl.window_should_close() {
+        boid.step();
+
         let mut draw = rl.begin_drawing(&thread);
 
         draw.clear_background(Color::WHITE);
-        draw.draw_text("Howdy, cowboy!", 12, 12, 20, Color::BLACK);
+        draw.draw_circle(boid.x % width, boid.y % height, 20.0, Color::BLACK);
     }
 }
