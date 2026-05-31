1ca0dedd2342112ce8f3cda1e5ce240a5b97d612
vincent benoot <vincent.benoot@gmail.com>
2026-05-31 07:21:16 +0200

    create separation function

--- src/boid.rs
+++ src/boid.rs
@@ -16,4 +16,16 @@ impl Boid {
         return (((self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y))
             as f32).sqrt();
     }
+
+    pub fn apply_separation(mut self, neighbours: Vec<&Boid>, factor: i32) {
+        let mut dx = 0;
+        let mut dy = 0;
+        for boid in neighbours {
+            dx += self.x - boid.x;
+            dy += self.y - boid.y;
+        }
+
+        self.speedx += factor * dx;
+        self.speedy += factor * dy;
+    }
 }
