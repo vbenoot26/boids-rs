32a24af5775d179b89dd53835ea8606622564a1e
vincent benoot <vincent.benoot@gmail.com>
2026-06-07 12:05:31 +0200

    create possible neighbours function

--- src/grid.rs
+++ src/grid.rs
@@ -66,4 +66,24 @@ impl Grid {
 
         (col as usize, row as usize)
     }
+
+    pub fn get_possible_neighbours(&self, boid: &Boid) -> Vec<BoidId> {
+        let (col_center, row_center) = self.get_idx(boid);
+
+        let col_min = col_center.max(0);
+        let col_max = col_center.min(self.rectangles[0].len());
+
+        let row_min = row_center.max(0);
+        let row_max = row_center.min(self.rectangles.len());
+
+        let mut result = Vec::new();
+
+        for col in col_min..col_max + 1 {
+            for row in row_min..row_max + 1 {
+                result.extend(self.rectangles[col][row].boids.iter().copied());
+            }
+        }
+
+        result
+    }
 }
