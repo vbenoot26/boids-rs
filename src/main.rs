73dfe58c1094bb013d1ad3ee5c2cacf85955cb28
vbenoot-itsme <vincent.benoot@itsme-id.com>
2026-06-19 13:03:35 +0200

    it doesnt work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use boids::{context, world};
-use raylib::prelude::{Color, RaylibDraw};
+use raylib::{
+    drawing::RaylibTextureModeExt,
+    prelude::{Color, RaylibDraw},
+};
 
 fn main() {
     let ctx = context::Context::default();
@@ -7,13 +10,18 @@ fn main() {
 
     let (mut rl, thread) = raylib::init().size(ctx.width, ctx.height).build();
 
+    let mut texture = rl
+        .load_render_texture(&thread, ctx.width as u32, ctx.height as u32)
+        .unwrap();
+
     rl.set_target_fps(60);
 
     while !rl.window_should_close() {
         world.step();
 
         let fps_text = format!("FPS: {}", rl.get_fps());
-        let mut draw = rl.begin_drawing(&thread);
+
+        let mut draw = rl.begin_texture_mode(&thread, &mut texture);
 
         draw.clear_background(Color::BLACK);
         world
@@ -21,6 +29,9 @@ fn main() {
             .iter()
             .for_each(|b| draw.draw_pixel(b.x as i32, b.y as i32, Color::WHITE));
 
+        drop(draw);
+
+        let mut draw = rl.begin_drawing(&thread);
         draw.draw_text(&fps_text, 10, 10, 20, Color::WHITE);
     }
 }
